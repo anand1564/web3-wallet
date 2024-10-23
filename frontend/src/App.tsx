828b52915a2543d494babb7b67f4cfacbf0bbde5
anand_code <anand8584pandey@gmail.com>
2024-10-23 22:35:35 +0530

    initial commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
 
+import { useState } from 'react'
+import { generateMnemonic } from 'bip39';
 import './App.css'
 
 function App() {
-
+const [menomic,setMnemonic] = useState<string>('');
+const handleGenerate=async ()=>{
+  const mn = await generateMnemonic();
+  setMnemonic(mn);
+}
   return (
-    <>
-    <h1 className='text-2xl font-bold text-center'>Hi There</h1>
-    </>
+    <div className='flex items-center justify-center w-full min-h-screen flex-col'>
+      <button className='text-xl bg-slate-900 text-white p-4 rounded-lg' onClick={handleGenerate} >Generate</button>
+      <input className='text-xl bg-slate-900 text-white p-4 rounded-lg' value={menomic} />
+    </div>
   )
 }
 
